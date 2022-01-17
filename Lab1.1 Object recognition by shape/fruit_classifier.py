d27a3761ac46415762e4412d3316e12eb91cc20f
MikhailShevchenko <bringmetohappiness@mail.ru>
2022-01-18 02:12:44 +0300

    упаковал код из ЛР1.1 в класс

---
+++ Lab1.1 Object recognition by shape/fruit_classifier.py
@@ -0,0 +1,199 @@
+"""Здесь определён простой классификатор, распознающий фрукты по форме."""
+import math
+import os
+
+import numpy as np
+from skimage import io
+from skimage import filters
+
+_EPSILON = 1e-8
+_BLACK_PIXEL = 0
+_WHITE_PIXEL = 255
+_CENTER = 49.5
+
+
+class FruitClassifier:
+    def train(self, path, bw_param, radius_param, variance_param):
+        """Перебором находит лучшие параметры для классификации фруктов, используя обучающий набор.
+
+        Args:
+            path: путь, по которому лежит обучающий набор данных.
+            bw_param: параметры перебора границы перевода изображения в чёрно-белый формат.
+            radius_param: параметры перебора радиуса, в котором не будут учитываться точки.
+            variance_param: параметры перебора границы дисперсии при предсказании.
+        Returns:
+            best_bw_threshold: лучший порог яркости для перевода в бинарное изображение.
+            best_radius: лучший радиус для подсчёта расстояний.
+            best_variance_threshold: лучший порог дисперсии для предсказания фрукта.
+        """
+        self.fruit1_name = os.listdir(path)[0]  # Ginger Root
+        self.fruit2_name = os.listdir(path)[1]  # Physalis
+
+        best_accuracy = 0
+
+        bw_threshold = bw_param[0]
+        while best_accuracy != 1 and abs(bw_threshold - bw_param[1]) > _EPSILON:
+            radius = radius_param[0]
+            while best_accuracy != 1 and radius != radius_param[1]:
+                variance_threshold = variance_param[0]
+                while best_accuracy != 1 and variance_threshold != variance_param[1]:
+                    tp, fn, tn, fp = self._tp_fn_tn_fp(
+                        path, bw_threshold, radius, variance_threshold)
+                    current_accuracy = self._accuracy(tp, fn, tn, fp)
+                    if best_accuracy < current_accuracy:
+                        best_accuracy = current_accuracy
+                        best_bw_threshold = bw_threshold
+                        best_radius = radius
+                        best_variance_threshold = variance_threshold
+                    if abs(best_accuracy - 1.0) < _EPSILON:
+                        best_accuracy = 1
+                    variance_threshold += variance_param[2]
+                radius += radius_param[2]
+            bw_threshold += bw_param[2]
+
+        self.bw_threshold = best_bw_threshold
+        self.radius = best_radius
+        self.variance_threshold = best_variance_threshold
+
+    @staticmethod
+    def _accuracy(tp, fn, tn, fp):
+        """Возвращает точность."""
+        accuracy = (tp + tn) / (tp + tn + fp + fn)
+        return accuracy
+
+    @staticmethod
+    def _precision(tp, fp):
+        """Возвращает прецизионность."""
+        precision = tp / (tp + fp)
+        return precision
+
+    @staticmethod
+    def _negative_predictive_value(tn, fn):
+        """Возвращает отрицательное прогностическое значение."""
+        negative_predictive_value = tn / (tn + fn)
+        return negative_predictive_value
+
+    @staticmethod
+    def _sensitivity(tp, fn):
+        """Возвращает чувствительность."""
+        sensitivity = tp / (tp + fn)
+        return sensitivity
+
+    @staticmethod
+    def _specificity(tn, fp):
+        """Возвращает специфичность."""
+        specificity = tn / (tn + fp)
+        return specificity
+
+    def _tp_fn_tn_fp(self, path, bw_threshold, radius, variance_threshold):
+        """Возвращает TruePositive, FalseNegative, TrueNegative и FalsePositive."""
+        tp, fn = self._get_tp_fn_or_tn_fp(
+            path, self.fruit1_name, bw_threshold, radius, variance_threshold)
+        tn, fp = self._get_tp_fn_or_tn_fp(
+            path, self.fruit2_name, bw_threshold, radius, variance_threshold)
+        return tp, fn, tn, fp
+
+    def _get_tp_fn_or_tn_fp(self, path, fruit_name, threshold_get_bin, radius, variance_threshold):
+        """Возвращает TruePositive и FalseNegatives или TrueNegatives и FalsePositives
+        в зависимости от предоставленного фрукта.
+
+        Если fruit_name == self.fruit1_name вернёт TruePositives и FalseNegatives.
+        Если fruit_name == self.fruit2_name вернёт TrueNegatives и FalsePositives.
+        """
+        filenames = os.listdir(os.path.join(path, fruit_name))
+        tp_or_tn = 0
+        fn_or_fp = 0
+        for filename in filenames:
+            if self.predict(
+                    os.path.join(path, fruit_name, filename),
+                    threshold_get_bin,
+                    variance_threshold,
+                    radius=radius) == fruit_name:
+                tp_or_tn += 1
+            else:
+                fn_or_fp += 1
+        return tp_or_tn, fn_or_fp
+
+    def predict(self, path, bw_threshold, variance_threshold, radius=0):
+        """Предсказывает, что за фрукт на изображении.
+
+        Args:
+            path: путь, по которому лежит изображение.
+            bw_threshold: значение порога яркости при переводе изображения в чёрно-белый формат.
+            radius: радиус окружности в центре изображения, в котором не нужно считать расстояния.
+            variance_threshold: значение порога вероятности для предсказания фрукта.
+        Returns:
+            predict: предсказание - название фрукта.
+        """
+        image = io.imread(path, as_gray=True)
+        image = filters.roberts(image)
+        image = self.black_whited(image, bw_threshold)
+        distances = self._get_distances(image, radius=radius)
+        variance = np.var(distances)
+        predict = self.fruit1_name if variance > variance_threshold else self.fruit2_name
+        return predict
+
+    @staticmethod
+    def black_whited(image, bw_threshold):
+        """Переводит изображение в оттенках серого в чёрно-белый формат.
+
+        Args:
+            image: изображение в оттенках серого.
+            bw_threshold: пороговое значение.
+        Returns:
+            bw_image: чёрно-белое изображение.
+        """
+        bw_image = []
+        for line in image:
+            bw_line = []
+            for pixel in line:
+                if pixel <= bw_threshold:
+                    bw_line.append(_WHITE_PIXEL)
+                else:
+                    bw_line.append(_BLACK_PIXEL)
+            bw_image.append(bw_line)
+        bw_image = np.array(bw_image)
+        return bw_image
+
+    @staticmethod
+    def _get_distances(image, radius=0):
+        """Возвращает список расстояний между белыми пикселями и центром чёрно-белого изображения.
+
+        Возвращает список расстояний между белыми пикселями и центром чёрно-белого изображения,
+        кроме тех пикселей, что лежат в центральной окружности.
+
+        Args:
+            image: чёрно-белое изображение.
+            radius: радиус окружности в центре изображения, в котором не нужно считать расстояния.
+        Returns:
+            distances: список расстояний между белыми пикселями и центром чёрно-белого изображения.
+        """
+        distances = []
+        for i in range(100):
+            for j in range(100):
+                if image[i][j] == _BLACK_PIXEL:
+                    distance = math.sqrt((i - _CENTER)**2 + (j - _CENTER)**2)
+                    if radius and distance >= radius:
+                        distances.append(distance)
+        distances = np.array(distances)
+        return distances
+
+    def test(self, path):
+        """Подсчитывает оценки классификации, используя найденные во время обучения на тестовом
+        наборе параметры.
+
+        Args:
+            path: путь, по которому лежит тестовый набор данных.
+        """
+        tp, fn, tn, fp = self._tp_fn_tn_fp(
+            path, self.bw_threshold, self.radius, self.variance_threshold)
+        accuracy = self._accuracy(path, self.bw_threshold, self.radius, self.variance_threshold)
+        precision = self._precision(tp, fp)
+        negative_predictive_value = self._negative_predictive_value(tn, fn)
+        sensitivity = self._sensitivity(tp, fn)
+        specificity = self._specificity(tn, fp)
+        print(
+            f'accuracy = {accuracy:4.2%}, precision = {precision:4.2%}'
+            f'negative predictive value = {negative_predictive_value:4.2%}'
+            f'sensitivity = {sensitivity:4.2%}'
+            f'specificity = {specificity:4.2%}')
--- Lab1.1 Object recognition by shape/report_lab1_1.py
+++ Lab1.1 Object recognition by shape/report_lab1_1.py
@@ -1,55 +1,49 @@
-"""Report for lab1 in Digital Image Processing."""
+"""Здесь написан скрипт, рисующий картинки для отчёта."""
 import os
 
 import matplotlib.pyplot as plt
 from skimage import color
 
-import lab1_1
+import fruit_classifier
 
-
-GINGER_ROOT_TRAIN = os.path.join(lab1_1.TRAIN_PATH, lab1_1.FRUIT1_NAME)
-PHYSALIS_TRAIN = os.path.join(lab1_1.TRAIN_PATH, lab1_1.FRUIT2_NAME)
+GINGER_ROOT_TRAIN = os.path.join(os.path.dirname(__file__), 'dataset', 'train', 'Ginger Root')
+PHYSALIS_TRAIN = os.path.join(os.path.dirname(__file__), 'dataset', 'train', 'Physalis')
 
 
 def show_fruit(path):
-    """Shows 9 examples of images in directory."""
-    filenames = os.listdir(path)
+    """Показывает 9 изображений из каталога."""
+    file_names = os.listdir(path)
     fig, axes = plt.subplots(3, 3)
     axes = axes.flatten()
-    for ax, filename in zip(axes, filenames[:9]):
-        image = plt.imread(os.path.join(path, filename))
+    for ax, file_name in zip(axes, file_names[:9]):
+        image = plt.imread(os.path.join(path, file_name))
         ax.imshow(image)
         ax.set_axis_off()
     fruit_name = os.path.basename(path)
-    fig.suptitle(f'Some of {fruit_name}')
+    fig.suptitle(f'Некоторые из {fruit_name}')
     plt.show()
 
 
-def get_first_image(path):
-    """Returns the first image in the directory."""
+def first_image(path):
+    """Возвращает первое изображение в директории."""
     filename = os.listdir(path)[0]
     image = plt.imread(os.path.join(path, filename))
     return image
 
 
 def show_processing(image):
-    """Shows Image Processing.
-
-    The first figure is an original image.
-    The second figure is grayscaled image.
-    The third figure is blackwhited image.
-    """
+    """Показывает обработку изображения."""
     gray_scale_image = color.rgb2gray(image)
-    black_white_image = lab1_1.get_bw_image(gray_scale_image, 0.4)
+    black_white_image = fruit_classifier.FruitClassifier.black_whited(gray_scale_image, 0.4)
 
     fig, axes = plt.subplots(1, 3, figsize=(9, 3))
     axes[0].imshow(image)
-    axes[0].set_title('Original Image')
+    axes[0].set_title('Исходное изображение')
     axes[1].imshow(gray_scale_image, cmap=plt.cm.gray)
-    axes[1].set_title('Grayscale Image')
+    axes[1].set_title('Изображение в оттенках серого')
     axes[2].imshow(black_white_image, cmap=plt.cm.gray)
-    axes[2].set_title('Black-white Image')
-    fig.suptitle('Image Processing', fontsize=16)
+    axes[2].set_title('Чёрно-белое изображение')
+    fig.suptitle('Обработка изображения', fontsize=16)
     plt.show()
 
 
@@ -57,7 +51,7 @@ def main():
     show_fruit(GINGER_ROOT_TRAIN)
     show_fruit(PHYSALIS_TRAIN)
 
-    image = get_first_image(GINGER_ROOT_TRAIN)
+    image = first_image(GINGER_ROOT_TRAIN)
     show_processing(image)
 
 
